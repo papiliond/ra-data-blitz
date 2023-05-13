dca49684c978bea25a043a53cbc18e5f5f3a09cc
Daniel Papilion <papiliond.94@gmail.com>
2023-05-13 18:56:28 +0200

    Change handleRoot base path to src/

--- test/setup.ts
+++ test/setup.ts
@@ -1,11 +1,11 @@
 export {};
 
-jest.mock('app/posts/queries/getPost', () => () => ({}), { virtual: true });
-jest.mock('app/posts/queries/getPosts', () => () => ({ posts: [], count: 0 }), { virtual: true });
-jest.mock('app/posts/mutations/createPost', () => () => ({}), { virtual: true });
-jest.mock('app/posts/mutations/updatePost', () => () => ({}), { virtual: true });
-jest.mock('app/posts/mutations/deletePost', () => () => ({}), { virtual: true });
-jest.mock('app/reactAdmin/posts/queries/getPost', () => () => ({}), { virtual: true });
+jest.mock('src/posts/queries/getPost', () => () => ({}), { virtual: true });
+jest.mock('src/posts/queries/getPosts', () => () => ({ posts: [], count: 0 }), { virtual: true });
+jest.mock('src/posts/mutations/createPost', () => () => ({}), { virtual: true });
+jest.mock('src/posts/mutations/updatePost', () => () => ({}), { virtual: true });
+jest.mock('src/posts/mutations/deletePost', () => () => ({}), { virtual: true });
+jest.mock('src/reactAdmin/posts/queries/getPost', () => () => ({}), { virtual: true });
 
 global.mockedInvoke = jest.fn(async (handler, params) => {
   return handler(params);
